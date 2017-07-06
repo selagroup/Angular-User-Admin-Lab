93fcf0b45f4e92acf248ad03a62c2a1cb9112cc4
Nir Noy <nirn@sela.co.il>
2017-07-06 18:02:50 +0300

    step 3

---
+++ src/step3/app/users/users-container/user-container.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {UserInfo, UserModel} from "../../models/user.model";
+
+@Component({
+  selector: 'app-user-container',
+  templateUrl: './user-container.component.html',
+  styleUrls: ['./user-container.component.css']
+})
+export class UserContainerComponent implements OnInit {
+
+  private currentUser:UserModel;
+  constructor() {
+
+    this.currentUser = new UserModel(0,'johndoe','johndoe@gmail.com',
+      new UserInfo('John','Doe','Clerk','http://authenticgoods.co/wrapbootstrap/themes/sparks/img/team/avatar-male.png'));
+
+  }
+
+
+  updateUser(username:string,email:string){
+    this.currentUser.email = email;
+    this.currentUser.username = username;
+  }
+
+  ngOnInit() {
+    console.log('User Container Init');
+  }
+
+}
