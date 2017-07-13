9f85e07e5d99778da66db68cf43914464e9ec037
Nir Noy <nirn@sela.co.il>
2017-07-13 23:57:47 +0300

    step 4

---
+++ src/step5/app/users/user-edit/user-edit.component.ts
@@ -0,0 +1,26 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {UserModel} from "../../models/user.model";
+
+@Component({
+  selector: 'app-user-edit',
+  templateUrl: './user-edit.component.html',
+  styleUrls: ['./user-edit.component.css']
+})
+export class UserEditComponent implements OnInit {
+
+  @Input()
+  user:UserModel;
+
+  @Output()
+  onUserUpdated:EventEmitter<UserModel> = new EventEmitter()
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  onSaveClicked(username:string,email:string){
+    this.onUserUpdated.emit(new UserModel(this.user.id,username,email));
+  }
+}
---
+++ src/step5/app/users/users-container/user-container.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import {UserInfo, UserModel} from "../../models/user.model";
+
+@Component({
+  selector: 'app-user-container',
+  templateUrl: './user-container.component.html',
+  styleUrls: ['./user-container.component.css']
+})
+export class UserContainerComponent implements OnInit {
+
+  private currentUser:UserModel;
+  private users:UserModel[];
+
+  constructor() {
+
+
+  }
+
+
+  updateUser(user:UserModel){
+    let inx = this.users.findIndex(u=> u.id === user.id);
+
+    if(inx>=0){
+
+      this.users[inx].username = user.username;
+      this.users[inx].email = user.email;
+    }
+
+  }
+
+  ngOnInit() {
+    console.log('User Container Init');
+
+
+    this.users =  [
+      new UserModel(0,'johndoe','johndoe@gmail.com',
+        new UserInfo('John','Doe','Clerk','http://authenticgoods.co/wrapbootstrap/themes/sparks/img/team/avatar-male.png')
+      ),
+      new UserModel(1,'janedoe','janedoe@gmail.com',
+        new UserInfo('Jane','Doe','Developer','http://brandonmathis.com/projects/fancy-avatars/demo/images/avatar_female_dark_on_clear_200x200.png')
+      ),
+      new UserModel(2,'johnsmith','johnsmith@gmail.com',
+        new UserInfo('John','Smith','Sales','http://authenticgoods.co/wrapbootstrap/themes/sparks/img/team/avatar-male.png')
+      ),
+      new UserModel(3,'janesmith','janesmith@gmail.com',
+        new UserInfo('Jane','DBA','Developer','http://brandonmathis.com/projects/fancy-avatars/demo/images/avatar_female_dark_on_clear_200x200.png')
+      ),
+
+
+    ]
+
+  }
+
+  onItemClicked(user:UserModel){
+    this.currentUser = user;
+  }
+
+}
