f798d7cd825295141ed12a6b9f7d73f0a9898c90
ri382j <ri382j@intl.att.com>
2018-07-05 12:47:29 +0300

    fix issues with the http module

--- src/step6/app/app.module.ts
+++ src/step6/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {UsersModule} from './users/users.module';
 
